da22395bba78ec20855d965073f1f3094f01ea38
jl-tab <jlco42257@gmail.com>
2024-09-26 20:52:17 -0500

    create header and change the web name

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Haedaer from './components/Header'
 
 function App() {
   return (
-    <>
-    </>
+    <div className='bg-slate-950 pt-1'>
+      <Haedaer/>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Haedaer() {
+  return (
+    <>
+      <nav className="container bg-slate-900 h-16 w-fit mx-auto rounded-3xl flex items-center text-slate-400">
+        <ul className="flex gap-5 font-medium">
+          <li className="hover:bg-slate-800 hover:text-white p-5 rounded-l-3xl">Inicio</li>
+          <li className="hover:bg-slate-800 hover:text-white p-5 ">Sobre mí</li>
+          <li className="hover:bg-slate-800 hover:text-white p-5 ">Proyectos</li>
+          <li className="hover:bg-slate-800 hover:text-white p-5 rounded-r-3xl">Contacto</li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Haedaer
